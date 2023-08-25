3d27707b3a9d59a5203057e64eb48d8aab8e672d
Samuel Ramesh <sampingu02@gmail.com>
2023-08-26 01:11:03 +0530

    Add "Login" and "Register" pages

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Twilite.Models;
+
+namespace Twilite.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(ILogger<AuthController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
