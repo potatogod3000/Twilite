adc41986e2c5e7ec4262089554143dbd3df986d9
Samuel Ramesh <sampingu02@gmail.com>
2023-08-29 21:54:04 +0530

    Register page visual error feedback changes

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -30,6 +30,10 @@ public class AuthController : Controller {
             return RedirectToAction("Index", "Home");            
         }
 
+        if(model.UserName.Contains(' ')) {
+            ModelState.AddModelError("", $"The User Name - \'{model.UserName}\' is invalid. Username cannot contain a Space.");
+        }
+
         return View();
     } */
 
@@ -41,8 +45,21 @@ public class AuthController : Controller {
     [HttpPost]
     public IActionResult Register(UserInfoModel model) {
 
+        List<string> invalidUserNames = new()
+            {"test", "test_user", "test.user", "test-user", "admin", "twilite", "twiliteadmin", "twilite-admin", "twilite.admin", "twilite_admin"};
+
         if(model.Password != null && model.UserName == model.Password) {
-            ModelState.AddModelError("password", "The User Name and the Password cannot be the same");
+            ModelState.AddModelError("password", "The User Name and the Password cannot be the same.");
+        }
+
+        for(int i = 0; i < invalidUserNames.Count; i++) {
+            if(model.UserName == "" && model.UserName.ToLower() == invalidUserNames[i]) {
+                ModelState.AddModelError("", $"The User Name - \'{model.UserName}\' is invalid. Username cannot contain \"Twilite\", \"Admin\" or \"Test\".");
+            }
+        }
+
+        if(model.UserName != null && model.UserName.Contains(' ')) {
+            ModelState.AddModelError("username", $"User Name cannot contain a Space.");
         }
 
         if(ModelState.IsValid) {
