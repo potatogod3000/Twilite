1979e8af412c20474ff209af3108c203da21378d
Samuel Ramesh <sampingu02@gmail.com>
2023-09-16 11:21:48 +0530

    Store the selected theme in localStorage

--- wwwroot/js/ThemeSwitcher.js
+++ wwwroot/js/ThemeSwitcher.js
@@ -3,21 +3,25 @@ var themeSwitcher = document.getElementById("theme-switcher-button");
 var lightThemeSelector = document.getElementById("light-theme");
 var darkThemeSelector = document.getElementById("dark-theme");
 
-document.addEventListener("DOMContentLoaded", () => {
-    if(rootElement.getAttribute("data-bs-theme") == "light") {
+document.addEventListener("readystatechange", () => {
+    if(localStorage.getItem("theme") == "light" || localStorage.getItem("theme") == undefined || localStorage.getItem("theme") == "") {
         themeSwitcher.innerHTML = "<i class='bi bi-brightness-high-fill mx-2'></i>";
+        rootElement.setAttribute("data-bs-theme", "light");
     }
-    else if(rootElement.getAttribute("data-bs-theme") == "dark") {
+    else if(localStorage.getItem("theme") == "dark") {
         themeSwitcher.innerHTML = "<i class='bi bi-moon-stars-fill mx-2'></i>";
+        rootElement.setAttribute("data-bs-theme", "dark");
     }
 });
 
 lightThemeSelector.addEventListener("click", () => {
     rootElement.setAttribute("data-bs-theme", "light");
     themeSwitcher.innerHTML = "<i class='bi bi-brightness-high-fill mx-2'></i>";
+    localStorage.setItem("theme", "light");
 });
 
 darkThemeSelector.addEventListener("click", () => {
     rootElement.setAttribute("data-bs-theme", "dark");
     themeSwitcher.innerHTML = "<i class='bi bi-moon-stars-fill mx-2'></i>";
+    localStorage.setItem("theme", "dark");
 });
