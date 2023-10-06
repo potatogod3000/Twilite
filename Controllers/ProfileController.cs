ada6c7a94a982bc8834cbff068e0eb99cbdd7c89
Samuel Ramesh <sampingu02@gmail.com>
2023-10-07 00:23:04 +0530

    Show replies sorted by posts in user's profile

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -16,15 +16,36 @@ public class ProfileController : Controller {
 
     [Authorize]
     [HttpGet]
-    public IActionResult UserProfile(string UserName) {
+    public async Task<IActionResult> UserProfile(string UserName) {
+        List<PostInfoModel> Posts = _db.Posts.Include(r => r.Replies).ToList();
+        List<ReplyInfoModel> Replies;
+        List<List<ReplyInfoModel>> RepliesList = new();
+        string comparison;
+        
         if(UserName == null) {
             ViewData["CurrentUserProfile"] = _db.UserProfiles.FirstOrDefault(x => x.UserName == User.Identity.Name);
             ViewData["CurrentUserPosts"] = _db.Posts.Where(x => x.UserName == User.Identity.Name).Include(r => r.Replies).ToList();
+            comparison = User.Identity.Name;
         }
         else {
             ViewData["CurrentUserProfile"] = _db.UserProfiles.FirstOrDefault(x => x.UserName == UserName);
             ViewData["CurrentUserPosts"] = _db.Posts.Where(x => x.UserName == UserName).Include(r => r.Replies).ToList();
+            comparison = UserName;
         }
+
+        foreach(var Post in Posts) {
+            Replies = new();
+            foreach(var Reply in Post.Replies) {
+                if(Reply.UserName == comparison) {
+                    Replies.Add(Reply);
+                }
+            }
+            if(Replies.Count != 0) {
+                RepliesList.Add(Replies);
+            }
+        }
+
+        ViewData["AllReplies"] = RepliesList;
 
         return View("UserProfile", ViewData);
     }
